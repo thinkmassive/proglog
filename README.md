c0d5d300b1c8a01b16fa5eac5e4a64f76c1935e2
Alex Miller <alex@thinkmassive.org>
2020-07-12 22:49:19 -0400

    fix json for correct tests

--- README.md
+++ README.md
@@ -16,8 +16,8 @@ curl -X POST http://localhost:8080 -d '{"record":{"value":"TGV0J3MgR28gIzMK"}}'
 3. Read records
 
 ```
-curl -X GET http://localhost:8080 -d '{"record":{"offset":"0"}}'
-curl -X GET http://localhost:8080 -d '{"record":{"offset":"1"}}'
-curl -X GET http://localhost:8080 -d '{"record":{"offset":"2"}}'
-curl -X GET http://localhost:8080 -d '{"record":{"offset":"3"}}'
+curl -X GET http://localhost:8080 -d '{"offset":0}'
+curl -X GET http://localhost:8080 -d '{"offset":1}'
+curl -X GET http://localhost:8080 -d '{"offset":2}'
+curl -X GET http://localhost:8080 -d '{"offset":9999}'
 ```
--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ curl -X POST http://localhost:8080 -d '{"record":{"value":"TGV0J3MgR28gIzIK"}}'
 curl -X POST http://localhost:8080 -d '{"record":{"value":"TGV0J3MgR28gIzMK"}}'
 
 echo "Reading records"
-curl -X GET http://localhost:8080 -d '{"record":{"offset":0}}'
-curl -X GET http://localhost:8080 -d '{"record":{"offset":1}}'
-curl -X GET http://localhost:8080 -d '{"record":{"offset":2}}'
-curl -X GET http://localhost:8080 -d '{"record":{"offset":3}}'
+curl -X GET http://localhost:8080 -d '{"offset":0}'
+curl -X GET http://localhost:8080 -d '{"offset":1}'
+curl -X GET http://localhost:8080 -d '{"offset":2}'
+curl -X GET http://localhost:8080 -d '{"offset":999999}'
